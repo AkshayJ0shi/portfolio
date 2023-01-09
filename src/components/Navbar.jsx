da28267ade07dc38fcd037e52dbc6bfa4b117a0d
Akshay Joshi <akshayjoshi1117@gmail.com>
2023-01-09 17:12:59 +0530

    resume button added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,20 @@
 import React from 'react'
 import "../index.css"
+import download from "../data/images/download.png"
 
 const Navbar = () => {
   return (
-    <header className='header'>
+    <header className='header' >
       <div className="container">
           <div className="navbar">
           <div className="logo">Portfolio</div>
 
           <div className="nav-links">
-              <a href="#projects" className="nav-link">Projects</a>
+              
               <a href="https://github.com/AkshayJ0shi?tab=repositories" className="nav-link">Github</a>
+              <a download={""} className="nav-link">Resume
+                <img src={download} alt="" />
+              </a>
           </div>
         </div>
       </div>
@@ -19,3 +23,9 @@ const Navbar = () => {
 }
 
 export default Navbar
+
+
+
+
+
+

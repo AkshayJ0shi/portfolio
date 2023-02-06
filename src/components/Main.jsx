0a7921f269f175fb3dedb0e1398e1026de13f003
Akshay Joshi <akshayjoshi1117@gmail.com>
2023-02-06 09:21:35 +0530

    change main skills

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -21,7 +21,7 @@ const Main = () => {
           <img src={psqlLogo} className="App-logo psql-logo" alt="logo" />
           <div className="line anim-typewriter">Hi, I am Akshay</div>
           <div className="line2 fadeIn">I am a Full Stack Developer</div>
-          <div className="line3 fadeIn">I mainly use React, NodeJS and Django</div>
+          <div className="line3 fadeIn">I mainly use React, Express and NestJS</div>
           <div className="line4 ">Check out my Projects</div>
         </div>
       </div>
